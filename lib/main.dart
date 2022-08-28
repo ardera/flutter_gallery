dba46747e36ffb6355a866ad918f5f573417bb81
Hannes Winkler <hanneswinkler2000@web.de>
2022-08-28 21:36:56 +0200

    make software mouse cursor look better

--- lib/main.dart
+++ lib/main.dart
@@ -20,17 +20,17 @@ import 'package:google_fonts/google_fonts.dart';
 export 'package:gallery/data/demos.dart' show pumpDeferredLibraries;
 
 class MouseCursorPainter extends CustomPainter {
-  MouseCursorPainter({required this.offset});
+  MouseCursorPainter({required this.offset, this.factor = 64.0});
 
   final Offset offset;
-  final factor = 32.0;
+  final double factor;
 
   @override
   void paint(Canvas canvas, Size size) {
     final path = ui.Path()
       ..moveTo(offset.dx, offset.dy)
-      ..lineTo(offset.dx, offset.dy + 48 / 64 * factor)
-      ..lineTo(offset.dx + 34 / 64 * factor, offset.dy + 34 / 64 * factor)
+      ..lineTo(offset.dx, offset.dy + 48 * factor / 64)
+      ..lineTo(offset.dx + 34 * factor / 64, offset.dy + 34 * factor / 64)
       ..close();
 
     canvas.drawPath(
@@ -46,7 +46,7 @@ class MouseCursorPainter extends CustomPainter {
         ..color = Colors.white
         ..style = ui.PaintingStyle.stroke
         ..strokeJoin = ui.StrokeJoin.miter
-        ..strokeWidth = 3.5 / 64 * factor,
+        ..strokeWidth = 3.5 * factor / 64,
     );
   }
 
@@ -90,7 +90,10 @@ class SoftwareMouseCursor extends StatelessWidget {
         valueListenable: cursorPos,
         builder: (context, pos, child) {
           return CustomPaint(
-            foregroundPainter: MouseCursorPainter(offset: pos),
+            foregroundPainter: MouseCursorPainter(
+              offset: pos,
+              factor: ui.window.devicePixelRatio * 64,
+            ),
             child: child,
           );
         },
